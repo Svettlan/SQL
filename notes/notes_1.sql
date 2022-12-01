a5e42acf9922b27351b99a93a09e2c2dbf3e1353
Svetlana <92315821+Svettlan@users.noreply.github.com>
2022-12-01 23:00:00 +0300

    Add files via upload

---
+++ notes/notes_1.sql
@@ -0,0 +1,98 @@
+--У какого композитора самый дорогой музыкальный трек? самый дешевый?
+select Composer,max(UnitPrice)
+from  tracks
+group by Composer
+order by max(UnitPrice) desc limit 10;
+
+--2 способ
+select Composer,max(UnitPrice)
+from  tracks;
+
+select Composer,min(UnitPrice)
+from  tracks;
+
+select Composer,min(UnitPrice)
+from  tracks
+group by Composer
+order by min(UnitPrice) asc limit 10;
+
+============================================================================
+--Вставить несколько строк в табл.,строки взяты из запроса к другой таблице
+Insert into media_types
+select *
+from genres
+where GenreId=26;
+--проверить, что получилось
+select *
+from media_types;
+
+===========================================================================
+--Чтобы случайно не удалить/изменить в таблице безвозвратно, нужно:
+
+begin transaction;-- открыть транзакцию
+
+DELETE-- сделать изменения 
+ FROM media_types
+ WHERE MediaTypeId = 127;
+
+select * --посмотреть результат
+from media_types;
+
+commit; --Применить изменения
+rollback; --Откатить обратно изменения
+
+============================================================================
+--Объединение таблиц
+
+ UNION; --объединение по вертикали
+         ALL     -- полный
+         DISTINCT --исключаюший дубликаты
+ ПОДЗАПРОСЫ
+         коррелированный  
+         без корреляции
+JOIN
+     CROSS  -- декартово произведение всех строк одной табл.со всеми строками др. табл.
+     INNER  -- внутреннее соединение
+     OUTER
+         LEFT  --левый
+         RIGHT --правый
+         FULL  --полный
+ EXCEPT    --логическое вычитание
+ INTERSECT --логическое пересечение                   
+         
+===============================================================================
+--UNION
+
+select FirstName,LastName,'клиент' from customers --создали искуственное поле "клиент"
+UNION ALL
+select FirstName,LastName, 'сотрудник' from employees;--создали искуственное поле "сотрудник"
+
+select FirstName from customers 
+UNION                             --подразумеват только уникальные значения distinct, отбрасывая дубликаты
+select FirstName from employees;
+
+================================================================================
+--ПОДЗАПРОСЫ
+
+select * from (
+select FirstName,LastName from customers -- подзапрос
+UNION                             
+select FirstName,LastName from employees)
+as t
+where FirstName='Aaron' ;           --условие
+
+=================================================================================
+-- EXCEPT
+
+select FirstName from customers 
+EXCEPT                           --выводит те данные из customers, которых нет в employees
+select FirstName from employees;
+
+=================================================================================
+--INTERSECT
+
+select FirstName from customers 
+INTERSECT                          --выводит данные, которые есть в обеих таблицах
+select FirstName from employees;
+
+=================================================================================
