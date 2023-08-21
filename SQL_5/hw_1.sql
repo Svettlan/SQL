9c8abb60c2bf33f62b5b37aa1e4f6048e1b773b9
Svetlana <92315821+Svettlan@users.noreply.github.com>
2023-08-21 22:27:39 +0300

    Update hw_1.sql

--- SQL_5/hw_1.sql
+++ SQL_5/hw_1.sql
@@ -1,33 +1,29 @@
 ========================================================================
 /*---------показать всех клиентов из Австралии---------------
 -------------show all customers in Australia-----------------*/
-  
-select * from classicmodels.customers
+  select * from classicmodels.customers
 where country = 'Australia';
+
 =========================================================================
 /*------------показать имя и фамилию клиентов в Мельбурне --------------
 ----------show First and Last name of customers in Melbourne ----------*/
-select Firstname and Lastname 
-from classicmodels.custome
-where city = 'Melbourne'
-
-
+select contactFirstName and contactLastName
+from classicmodels.customers
+where city = 'Melbourne';
 
 =========================================================================
 /*показать всех клиентов с кредитным лимитом более 200 000 долларов США
 show all customers with Credit Limit over $200,000*/
-
-select * from classicmodels.customers 
-where creditlimit > 200000 ;
-
-
+select * 
+from classicmodels.customers 
+where creditLimit > 200000 ;
 
 ==========================================================================
 /*кто президент компании?
 who is the president of the company?*/
-select * from classicmodels.employees
+select * 
+from classicmodels.employees
 where jobTitle = 'President';
-
 
 =========================================================================
 /*сколько торговых представителей в компании?
@@ -36,40 +32,36 @@ select count(*)
 from classicmodels.employees
 where jobTitle = 'Sales Reps';
 
-
 ===========================================================================
 /*показать платежи в порядке убывания
 show payments in descending order*/
-select * from classicmodels.payments
-order by payments desc;
-
-
+select * 
+from classicmodels.payments
+order by amount desc;
 
 ===========================================================================
 /*какой номер чека для платежа, сделанного 17 декабря 2004 г.
 what was the check# for the payment done on December 17th 2004*/
-select paymentnumber from classicmodels.payments
+select checkNumber 
+from classicmodels.payments
 where paymentDate = '2004-12-17';
-  
-
-
 
 ==========================================================================
 /*показать продуктовую линейку со словом «реалистичная» в описании
 show product line with the word 'realistic' in the description*/
-select * ProductLines
-from classicmodels.product
-where   productline like  '%realistic%';
-
+select  productLine
+from classicmodels.products
+where   productline like  'realistic';
 
 ==========================================================================
 /*показать название продукта для продавца "Unimax Art Galleries"
 show product name for vendor 'Unimax Art Galleries'*/
-
-select productName from classicmodel.products
-where vendor = 'Unimax Art Galleries';
+select productName from classicmodels.products
+where productVendor like 'Unimax Art Galleries';
 
 ==========================================================================
+/*какой номер клиента для наибольшей суммы платежа
+what is the customer number for the highest amount of payment*/
 
 
 
