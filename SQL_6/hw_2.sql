58375b967d6829196a538a8b16e2195413e2fb61
Svetlana <92315821+Svettlan@users.noreply.github.com>
2023-08-24 21:37:48 +0300

    Add files via upload

---
+++ SQL_6/hw_2.sql
@@ -0,0 +1,43 @@
+/*
+Homework #3  
+Part 1 - mywork database
+Write sql 
+	1. Add column 'country' to dept table in mywork database
+	2. Rename column 'loc' to 'city'
+	3. Add three more departments: HR, Engineering, Marketing
+	4. Write sql statement to show which department is in Atlanta
+    Save your work 
+
+
+Part 2  - library_simple database
+Run library_simple.sql script  (takes a few minutes)
+(source: https://github.com/amyasnov/stepic-db-intro/tree/2650f9a7f9c72e1219ea93cb4c4e410cca046e54/test)
+
+Look at table relationships in EER Diagram
+
+Write sql 
+	1. What is the first name of the author with the last name Swanson?
+	2. How many pages are in Men Without Fear book?
+	3. Show all book categories that start with with letter 'W'
+*/
+
+/*Часть 1 — база данных mywork
+Написать sql
+1. Добавьте столбец «страна» в таблицу отделов в базе данных mywork.
+2. Переименуйте столбец «loc» в «city».
+3. Добавьте еще три отдела: HR, Engineering, Marketing.
+4. Напишите оператор sql, чтобы показать, какой отдел находится в Атланте.
+    Сохраните свою работу
+
+
+Часть 2 — база данных Library_simple
+Запустите скрипт Library_simple.sql (займет несколько минут).
+(источник: https://github.com/amyasnov/stepic-db-intro/tree/2650f9a7f9c72e1219ea93cb4c4e410cca046e54/test)
+
+Посмотрите на отношения таблиц в диаграмме EER.
+
+Написать sql
+1. Как зовут автора с фамилией Суонсон?
+2. Сколько страниц в книге «Мужчины без страха»?
+3. Показать все категории книг, начинающиеся с буквы «W».*/
+-- ---------------------------------------------------
