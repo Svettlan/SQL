9df242911fd084e373b8d8c16e42f3ccf06b2be1
Svetlana <92315821+Svettlan@users.noreply.github.com>
2023-09-07 12:43:11 +0300

    Update RedRover.school_hw_2.sql

--- SQL_5/RedRover.school_hw_2.sql
+++ SQL_5/RedRover.school_hw_2.sql
@@ -53,6 +53,94 @@ select CONCAT (LastName,' ' , FirstName) as Сотрудник, Title
 from chinook.employee 
 where Title in ('Sales Manager', 'IT Manager');
 =========================================================================================================
+=========================================================================================================
+-- Покажите клиентов с именем Френк.
+select * 
+from chinook.customer
+where FirstName = 'Frank';
+=====================================================================================
+-- Покажите фамилии и имя клиентов, у которых имя Mарк.
+select * 
+from chinook.customer
+where FirstName = 'Mark';
+=====================================================================================
+-- Покажите название и размер треков в Мегабайтах, применив округление до 2 знаков и отсортировав по убыванию. Столбец назовите MB.
+select Name, round((Bytes/1024/1024),2) as MB
+from chinook.track
+order by Bytes desc;
+=====================================================================================
+-- Покажите возраст сотрудников, на момент оформления на работу. Вывести фамилию, имя, возраст. дату оформления и день рождения.
+-- Столбец назовите age.*
+ =============================
+ -------------------------------
+-- ------РАБОТА С ДАТАМИ-------
+ -------------------------------
+ =============================
+-- Один из вариантов решения задачи здесь: https://dev.mysql.com/doc/refman/8.0/en/date-and-time-functions.html#function_datediff   
+select LastName, FirstName, floor((datediff(HireDate, BirthDate))/365) as AgeDate, HireDate, BirthDate
+from chinook.employee;
+=====================================================================================
+-- Покажите покупателей-американцев без факса.
+select *
+from chinook.customer
+where Country = 'USA' and Fax is NULL;
+======================================================================================
+-- Покажите почтовые адреса клиентов из домена gmail.com.
+select *
+from chinook.customer
+where Email like '%gmail.com';
+=================================
+-- Покажите в алфавитном порядке все уникальные должности в компании.
+select distinct (Title)
+from chinook.employee
+order by Title asc;
+=======================================================================================
+-- Покажите название самой короткой песни.
+select Name 
+from chinook.track
+order by Milliseconds asc
+limit 1;
+==========================================
+-- Покажите название и длительность в секундах самой короткой песни. Столбец назвать sec.
+select Name, round((Milliseconds/1000)) as sec
+from chinook.track
+order by Milliseconds asc
+limit 1;
+========================================================================================
+-- Покажите средний возраст сотрудников, работающих в компании*.
+select round(avg(datediff(HireDate,BirthDate)/365)) as age
+from chinook.employee;
+========================================================================================
+-- Проведите аналитическую работу: узнайте фамилию, имя и компанию покупателя (номер 5). 
+-- Сколько заказов он сделал и на какую сумму.
+--  Покажите 2 запроса Вашей работы.
+select CustomerId, LastName, FirstName, Company
+from chinook.customer
+where CustomerId = 5;
+==============================
+select *
+from chinook.invoice
+where CustomerId = 5;
+select count(*), sum(Total)
+from chinook.invoice
+where CustomerId = 5;
+=========================================================================================
+-- Напишите запрос, определяющий количество треков, где ID плейлиста > 15.
+-- Назовите столбцы соответственно их расположения. 
+select PlaylistId as 'CONDTTION', count(TrackId) as RESULT
+from chinook.playlisttrack
+where PlaylistId > 15
+group by PlaylistId;
+============================
+select  *
+from chinook.playlisttrack;
+=========================================================================================
+-- Найти все ID счет-фактур, в которых количество товаров больше или равно 6 и меньше или равно 9.
+select InvoiceId, sum(Quantity)
+from chinook.invoiceline
+group by InvoiceId
+-- having sum(Quantity) >=6 and sum(Quantity) <= 9 
+having sum(Quantity) between 6 and 9;
 
 
 
